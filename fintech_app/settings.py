e2afa8307cfae9bbfe36e03f16d29854954a3d9a
anjoo91 <andrew.w.lew@gmail.com>
2023-09-13 18:11:57 -0400

    added custom user model to user_auths

--- fintech_app/settings.py
+++ fintech_app/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Custom User Model
+AUTH_USER_MODEL = 'user_auths.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
