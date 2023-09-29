f95e74381a70c3326eaee84ea15217930101d690
anjoo91 <andrew.w.lew@gmail.com>
2023-09-28 23:58:15 -0400

    updated form to check for existing accounts by referencing account number, id, and ref codes instead of pk; fixed short uuid fields not being populated on the form level in the admin dashboard

--- account/forms.py
+++ account/forms.py
@@ -9,7 +9,12 @@ class AccountAdminForm(forms.ModelForm):
         
     def __init__(self, *args, **kwargs):
         super(AccountAdminForm, self).__init__(*args, **kwargs)
-        if not self.instance.pk:  # Only set if creating a new object
+        # if these fields are empty, generate a ShortUUID of specific lengths for each field (new account)
+        # otherwise, use the value already in the field (existing account)
+        if not self.instance.account_number:
             self.initial['account_number'] = ShortUUID().random(length=10)
+        if not self.instance.account_id:
             self.initial['account_id'] = ShortUUID().random(length=7)
+        if not self.instance.ref_code:
             self.initial['ref_code'] = ShortUUID().random(length=10)
+
