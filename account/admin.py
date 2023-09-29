be8fd8f0befb91438492a536987363731514331f
anjoo91 <andrew.w.lew@gmail.com>
2023-09-28 23:02:56 -0400

    trimmed packages imported in admin.py for account admin dashboard; added KYCAdmin model

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
-from requests import request
 from models import Account
 from user_auths.models import User
-from django.utils.html import format_html
 from import_export.admin import ImportExportModelAdmin
-from sorl.thumbnail import get_thumbnail
-from django.utils.safestring import mark_safe
-from django import forms
+
 
 # Register your models here.
 class AccountAdminModel(ImportExportModelAdmin):
@@ -16,4 +12,9 @@ class AccountAdminModel(ImportExportModelAdmin):
     list_editable = ["account_status", "account_balence"]
     list_display = ["user", "account_number", "account_status", "account_balance"]
     list_filter = ["account_status"]
+
+class KYCAdmin(ImportExportModelAdmin):
+    model = Account
+    search_fields = ["full_name"]
+    list_display = ["user", "full_name"]
     
