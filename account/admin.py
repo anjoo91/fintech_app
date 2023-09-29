e22b656c9a8a5f3b25319efb123de60b8b003664
anjoo91 <andrew.w.lew@gmail.com>
2023-09-28 23:01:14 -0400

    added library packages for admin.py for accounts; crated account admin model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from requests import request
+from models import Account
+from user_auths.models import User
+from django.utils.html import format_html
+from import_export.admin import ImportExportModelAdmin
+from sorl.thumbnail import get_thumbnail
+from django.utils.safestring import mark_safe
+from django import forms
+
 # Register your models here.
+class AccountAdminModel(ImportExportModelAdmin):
+    model = Account
+    search_fields = ["full_name"]
+    list_editable = ["account_status", "account_balence"]
+    list_display = ["user", "account_number", "account_status", "account_balance"]
+    list_filter = ["account_status"]
+    
