ce0d6df46ca775e3bc7b24d4fe6e5e1dcb87a48b
anjoo91 <andrew.w.lew@gmail.com>
2023-09-28 23:04:14 -0400

    commented out KYC temporarily; will create KYC later

--- account/admin.py
+++ account/admin.py
@@ -7,14 +7,14 @@ from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 class AccountAdminModel(ImportExportModelAdmin):
-    model = Account
-    search_fields = ["full_name"]
-    list_editable = ["account_status", "account_balence"]
+    list_editable = ["account_status", "account_balance"]
     list_display = ["user", "account_number", "account_status", "account_balance"]
     list_filter = ["account_status"]
 
-class KYCAdmin(ImportExportModelAdmin):
-    model = Account
-    search_fields = ["full_name"]
-    list_display = ["user", "full_name"]
+# class KYCAdmin(ImportExportModelAdmin):
+#     model = Account
+#     search_fields = ["full_name"]
+#     list_display = ["user", "full_name"]
     
+admin.site.register(Account, AccountAdminModel)
+# admin.site.register(KYC, KYCAdmin)
