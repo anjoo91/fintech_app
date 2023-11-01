0ac5f52c17c3e9ed88b75149291f41811acd4ac6
anjoo91 <andrew.w.lew@gmail.com>
2023-10-31 23:30:20 -0400

    added user_auths namespace to main app urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    
+    path('user-auths/', include('user_auths.urls', namespace='user_auths')),
+
 ]
